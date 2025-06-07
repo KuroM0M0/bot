50da0484f3e2eab193e7c5c386d5c2992cb89fb4
Yanghaee <100843874+Yanghaee@users.noreply.github.com>
2025-06-07 04:22:47 +0200

    add: adding main Files

--- Events/ready.py
+++ Events/ready.py
@@ -1,4 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+# Lade Umgebungsvariablen aus der .env-Datei
+load_dotenv()
+YOUR_CHANNEL_ID = os.getenv("YOUR_CHANNEL_ID")
+
 def setup(bot):
     @bot.event
     async def on_ready():
+        channel = bot.get_channel(YOUR_CHANNEL_ID)
+        start_webhook_server(bot, channel)
         print(f"{bot.user} ist bereit!")
